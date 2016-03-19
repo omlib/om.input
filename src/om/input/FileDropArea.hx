3030293ca1df1ad4a59222320537804a685e6b13
Tong <tong@disktree.net>
2016-03-19 17:41:39 +0100

    FileDropArea report all drag events and cleanup

--- src/om/input/FileDropArea.hx
+++ src/om/input/FileDropArea.hx
@@ -1,55 +1,88 @@
 package om.input;
 
 import js.Browser.document;
+import js.html.DragEvent;
 import js.html.Element;
-import js.html.FileList;
+//import js.html.FileList;
 
-class FileDropArea {
+class FileDropArea implements om.Disposable {
 
-	public dynamic function onInput( files : FileList ) {}
+	public dynamic function onEvent( e : DragEvent ) {}
 
 	public var target(default,null) : Element;
 	public var active(default,null) = false;
 
-	public function new( ?target : Element ) {
-		this.target = (target == null) ? document.body : target;
+	public function new( target : Element ) {
+		this.target = target;
 	}
 
-	public function start() : FileDropArea {
+	public function activate() {
+
+		if( active )
+			return;
+
+		target.addEventListener( 'drag', handleDrag, false );
+		target.addEventListener( 'dragstart', handleDragStart, false );
+		target.addEventListener( 'dragend', handleDragEnd, false );
+		target.addEventListener( 'dragover', handleDragOver, false );
+		target.addEventListener( 'dragenter', handleDragEnter, false );
+		target.addEventListener( 'dragleave', handleDragLeave, false );
+		target.addEventListener( 'drop', handleDrop, false );
+
 		active = true;
-		target.addEventListener( 'dragover', onDragOver, false );
-		target.addEventListener( 'dragenter', onDragEnter, false );
-		target.addEventListener( 'drop', onDrop, false );
-		return this;
 	}
 
-	public function stop() : FileDropArea {
+	public function dispose() {
+
+		if( !active )
+			return;
+
+		target.removeEventListener( 'drag', handleDrag );
+		target.removeEventListener( 'dragstart', handleDragStart );
+		target.removeEventListener( 'dragend', handleDragEnd );
+		target.removeEventListener( 'dragover', handleDragOver );
+		target.removeEventListener( 'dragenter', handleDragEnter );
+		target.removeEventListener( 'dragleave', handleDragLeave );
+		target.removeEventListener( 'drop', handleDrop );
+
 		active = false;
-		target.removeEventListener( 'dragover', onDragOver );
-		target.removeEventListener( 'dragenter', onDragEnter );
-		target.removeEventListener( 'drop', onDrop );
-		return this;
 	}
 
-	function onDragOver(e) {
+	function handleDrag( e : DragEvent ) onEvent(e);
+	function handleDragStart( e : DragEvent ) onEvent(e);
+	function handleDragEnd( e : DragEvent ) onEvent( e );
+	function handleDragLeave( e : DragEvent ) onEvent( e );
+
+	function handleDragOver( e : DragEvent ) {
+
 		e.stopPropagation();
 		e.preventDefault();
+
+		onEvent( e );
+
 		return false;
 	}
 
-	function onDragEnter(e) {
+	function handleDragEnter( e : DragEvent ) {
+
 		e.stopPropagation();
 		e.preventDefault();
+
+		onEvent( e );
+
 		return false;
 	}
 
-	function onDrop(e) {
+
+	function handleDrop( e : DragEvent ) {
+
 		e.stopPropagation();
     	e.preventDefault();
+
 		//e.dataTransfer.dropEffect = 'copy';
-		//$type(e.dataTransfer);
 		//var files = e.dataTransfer.files;
-		onInput( e.dataTransfer.files );
+
+		onEvent( e );
 	}
 
 }
